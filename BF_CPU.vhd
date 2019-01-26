cbcfc2cded7c96abf41981438ed969d1d09e3968
Jojojoppe <joppe@blondel.nl>
2019-01-26 13:49:20 +0100

    Added working RAM
    Signed-off-by: Jojojoppe <joppe@blondel.nl>

--- BF_CPU.vhd
+++ BF_CPU.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 use work.all;
 
 entity BF_CPU is
@@ -8,46 +9,36 @@ entity BF_CPU is
 		RST			: in std_logic;
 
 		-- Debug output
-		LED			: out std_logic_vector(7 downto 0)
+		LED			: out std_logic_vector(7 downto 0);
+		D_clk		: out std_logic
 	);
 end entity;
 
 architecture a of BF_CPU is
 	signal nRST		: std_logic;
-	signal D		: std_logic_vector(7 downto 0);
-	signal A		: std_logic_vector(31 downto 0);
-	signal RAM_wr	: std_logic;
-	signal RAM_rd	: std_logic;
-	signal IO_wr	: std_logic;
-	signal IO_rd	: std_logic;
-	signal HLT		: std_logic;
+	
+	signal A		: std_logic_vector(15 downto 0);
 
 	signal sCLK		: std_logic;
-	signal IO0		: std_logic_vector(7 downto 0);
 
 begin
 
+	D_clk <= sCLK;
 	nRST <= not(RST);
 
 	e_FDIV : entity FDIV(a) generic map(100000000, 2)
 		port map(CLK, sCLK, nRST);
 
+	e_RAM : entity ip_RAM(ip_RAM_a)
+		port map(CLK, nRST, sCLK, "0", A, x"00", LED);
 
---	-- RAM
---	e_RAM : entity RAM(a)
---		port map(sCLK, nRST, RAM_rd, RAM_wr, D, D, A);
---
---	-- CPU
---	e_CPU : entity CPU(a)
---		port map(sCLK, nRST, D, D, A, RAM_wr, RAM_rd, IO_wr, IO_rd, HLT);
---
---	-- IO0
---	e_IO0 : entity REG8(a)
---		port map(sCLK, nRST, IO_rd, IO_wr, D, D, IO0);
---
-
-
-	LED(0) <= sCLK;
-	LED(7 downto 1) <= "0000000";
+	p_cnt : process(nRST, sCLK)
+	begin
+		if nRST='1' then
+			A <= x"0000";
+		elsif rising_edge(sCLK) then
+			A <= std_logic_vector(unsigned(A)+1);
+		end if;
+	end process;
 
 end architecture;
---
+++ ipcore_dir/ip_RAM_xmdf.tcl
@@ -0,0 +1,91 @@
+# The package naming convention is <core_name>_xmdf
+package provide ip_RAM_xmdf 1.0
+
+# This includes some utilities that support common XMDF operations
+package require utilities_xmdf
+
+# Define a namespace for this package. The name of the name space
+# is <core_name>_xmdf
+namespace eval ::ip_RAM_xmdf {
+# Use this to define any statics
+}
+
+# Function called by client to rebuild the params and port arrays
+# Optional when the use context does not require the param or ports
+# arrays to be available.
+proc ::ip_RAM_xmdf::xmdfInit { instance } {
+# Variable containing name of library into which module is compiled
+# Recommendation: <module_name>
+# Required
+utilities_xmdf::xmdfSetData $instance Module Attributes Name ip_RAM
+}
+# ::ip_RAM_xmdf::xmdfInit
+
+# Function called by client to fill in all the xmdf* data variables
+# based on the current settings of the parameters
+proc ::ip_RAM_xmdf::xmdfApplyParams { instance } {
+
+set fcount 0
+# Array containing libraries that are assumed to exist
+# Examples include unisim and xilinxcorelib
+# Optional
+# In this example, we assume that the unisim library will
+# be available to the simulation and synthesis tool
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type logical_library
+utilities_xmdf::xmdfSetData $instance FileSet $fcount logical_library unisim
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM/blk_mem_gen_v7_3_readme.txt
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM/doc/blk_mem_gen_v7_3_vinfo.html
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM/doc/pg058-blk-mem-gen.pdf
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM.asy
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM.mif
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM.ngc
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM.sym
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type symbol
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type vhdl
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM.vho
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type vhdl_template
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM.xco
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type coregen_ip
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ip_RAM_xmdf.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path summary.log
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount associated_module ip_RAM
+incr fcount
+
+}
+
+# ::gen_comp_name_xmdf::xmdfApplyParams
